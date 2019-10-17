63db1c5a49167e761b2e94352588d724bd91d3ca
GyuReeKim <starkim96@naver.com>
2019-10-17 11:14:45 +0900

    login

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+# from IPython import embed
 
 # Create your views here.
+def index(request):
+    # # embed()
+    # visit_num = request.session.get('visit_num', 0)
+    # # 방문 할 때마다 visit이 1씩 늘어남
+    # request.session['visit_num'] = visit_num + 1
+    
+    # request.session.modified = True
+
+    # context = {
+    #     'visit_num': visit_num,
+    # }
+
+    return render(request, 'todos/index.html')
