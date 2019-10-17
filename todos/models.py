e0d5c87338cfa01b278b27aaaa7aa9fe8c53ef26
GyuReeKim <starkim96@naver.com>
2019-10-17 11:31:55 +0900

    Todo 모델 정의

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Todo(models.Model):
+    content = models.CharField(max_length=100)
+    due_date = models.DateField()
+    created_at = models.DateField(auto_now_add=True) # 현재시간 저장
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
